0ffc7c8f475e5ee413fa5adc78d7b38bef86c260
Colderz <arekzim98@gmail.com>
2021-01-11 22:01:02 +0100

    Implement DAO interface

---
+++ app/src/main/java/pakiet/arkadiuszzimny/newsapp/db/ArticleDAO.kt
@@ -0,0 +1,21 @@
+package pakiet.arkadiuszzimny.newsapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import pakiet.arkadiuszzimny.newsapp.Article
+
+@Dao
+interface ArticleDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    //update and insert
+    suspend fun upsert(article: Article): Long
+
+
+    //Nie będzie suspend, bo zwraca obkiety LiveData
+    @Query("SELECT * FROM articles")
+    fun getAllArticles() : LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
