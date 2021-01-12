5804c21eaba6d2e576146d98a9bf7475adefd9e8
Colderz <arekzim98@gmail.com>
2021-01-12 19:15:19 +0100

    BreakingNewsFragment done

--- app/src/main/java/pakiet/arkadiuszzimny/newsapp/repository/NewsRepository.kt
+++ app/src/main/java/pakiet/arkadiuszzimny/newsapp/repository/NewsRepository.kt
@@ -1,8 +1,12 @@
 package pakiet.arkadiuszzimny.newsapp.repository
 
+import pakiet.arkadiuszzimny.newsapp.api.RetrofitInstance
 import pakiet.arkadiuszzimny.newsapp.db.ArticleDatabase
 
 class NewsRepository(
     val db: ArticleDatabase
 ) {
+
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
 }
